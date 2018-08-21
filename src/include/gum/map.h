5465df208e5ec305ddd152e86e82f48623e86ea2
Joshua Minter <josh@minteronline.com>
2018-08-22 09:34:38 +1000

    Created map definition

--- src/include/gum/map.h
+++ src/include/gum/map.h
@@ -1,7 +1,27 @@
 #ifndef GUM_MAP_H_
 #define GUM_MAP_H_
 #include "../gum.h"
+#include "array.h"
 
+typedef struct gum_mapiter_t {
+	gum_int_t index;
+	const char* key;
+	void* value;
+} gum_mapiter_t;
 
+typedef struct gum_map_t {
+	gum_array_t array;
+	gum_int_t value_size;
+	gum_int_t size;
+} gum_map_t;
+
+GUM_API void gum_map_create(gum_map_t* map, gum_int_t value_size);
+GUM_API void gum_map_destroy(gum_map_t* map);
+GUM_API void* gum_map_add(gum_map_t* map, const char* key);
+GUM_API void gum_map_remove(gum_map_t* map, const char* key);
+GUM_API void* gum_map_get(gum_map_t* map, const char* key);
+
+GUM_API void gum_mapiter_create(gum_map_t* map, gum_mapiter_t* iter);
+GUM_API _Bool gum_mapiter_next(gum_map_t* map, gum_mapiter_t* iter);
 
 #endif
